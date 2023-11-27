aea67c59a0ab115e3fe7d99980d90673d58cb13b
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2023-11-27 14:20:38 +0100

    config

--- news_tracker_app.py
+++ news_tracker_app.py
@@ -238,11 +238,11 @@ def define_research():
             # Load the selected configuration into session state
             st.session_state['research_type'] = selected_config['Research Type']
             st.session_state['sources'] = selected_config['Sources']
+            st.session_state['limit_to_country'] = parse_boolean(selected_config['Limit to Country'])
             st.session_state['official_sources'] = selected_config['Official Sources'].split(';')
             st.session_state['selected_mandatory_keywords'] = selected_config['Mandatory Keywords'].split(';')
             st.session_state['selected_keywords'] = selected_config['Topic Keywords'].split(';')
             st.session_state['selected_comp_keywords'] = selected_config['Complementary Keywords'].split(';')
-            st.session_state['selected_countries'] = selected_config['Selected Countries'].split(';')
 
         if config_choice == "Custom Configuration":    
             for key, value in defaults.items():
