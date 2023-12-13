3017160fe91c3f38e1a1154d547aa654ec3d2c37
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2023-12-13 08:52:25 +0100

    write

--- news_tracker_app.py
+++ news_tracker_app.py
@@ -841,8 +841,8 @@ def research():
     if st.sidebar.button("Run Research"):
         query, total_query_elements = construct_query()
         max_parameters_per_query = 32
-        write(total_query_elements)
-        write(query)
+        st.write(total_query_elements)
+        st.write(query)
 
         # Function to break down the links into chunks of size n
         def chunk_list(lst, n):
